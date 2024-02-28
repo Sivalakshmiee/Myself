c35cdcd99d07c9d57a36621b74eb8f04cf72b42b
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-02-28 10:22:26 +0530

    Literal

---
+++ java/Literal.java
@@ -0,0 +1,15 @@
+public class Literal {
+
+    public static void main(String[] args) {
+        int num1 = 100_00_000;
+        System.out.println(num1);
+
+        double num = 12e10;
+        System.out.println(num);
+
+        char c = 'a';
+        c++;
+        System.out.println(c);
+    }
+
+}
