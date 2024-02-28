f51088dd9e5d1dd26759b95d80b7be5568734b6e
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-02-28 10:13:52 +0530

    JL

---
+++ java/Data.java
@@ -0,0 +1,25 @@
+class Data {
+    // primitive - int, float, characters, boolean
+
+    public static void main(String[] args) {
+        int n = 8;
+        byte by = 127;
+        short sh = 455;
+        long l = 8654l;
+        float marks = 0.8f; // default - double
+        double num = 98.6;
+        char c = 'a';
+        boolean b = true;
+
+        System.out.println(n);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(marks);
+        System.out.println(num);
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+
+}
