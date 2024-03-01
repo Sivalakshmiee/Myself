1d0d5e936ee8f602f19ce34c8f28f7907383093a
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-01 11:11:57 +0530

    Add files via upload

---
+++ java/Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 7;
+        // int num2 = 6;
+
+        // int result = num1 % num2;
+
+        // num1 = num1 + 2;
+
+        // num1 *= 2;
+
+        // num1++;
+        // num1--;
+        // ++num1;
+        int res = num1++;
+
+        System.out.println(res); // Output: 13
+    }
+}
