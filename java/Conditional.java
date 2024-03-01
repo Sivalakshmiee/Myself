01e0b42536c4922f86ef7dbcb1834c43b501339e
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-01 12:02:46 +0530

    Add files via upload

---
+++ java/Conditional.java
@@ -0,0 +1,14 @@
+public class Conditional {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 13;
+
+        if (x > y && x <= y) {
+            System.out.println("x is greater than 5");
+        } else {
+            System.out.println("x is not greater than 5");
+        }
+
+    }
+}
