c7ee5aa2bb3ecfd9b6de74d342185f5a87f09ffb
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-01 10:42:39 +0530

    Add files via upload

---
+++ java/Type.java
@@ -0,0 +1,12 @@
+public class Type {
+    public static void main(String args[]) {
+        // byte b = 127;
+        int a = 12;
+        byte k = (byte) a;
+
+        float f = 6.6f;
+        int i = (int) f;
+
+        System.out.println(i);
+    }
+}
