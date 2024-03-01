65555cba3ffe8ec47d2cb2e934d6b88c103bbb54
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-01 14:52:36 +0530

    Add files via upload

---
+++ java/Ternery.java
@@ -0,0 +1,11 @@
+public class Ternery {
+    public static void main(String[] args) {
+        int res = 0;
+        int n = 11;
+
+        res = n % 2 == 0 ? 1 : 0;
+
+        System.out.println(res);
+
+    }
+}
