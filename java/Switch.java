1279469ffc53fb21b6de60a5ee711a1ea2e9404d
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-01 15:15:41 +0530

    Add files via upload

---
+++ java/Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+    public static void main(String[] args) {
+        int n = 1;
+        switch (n) {
+            case 1:
+                System.out.println("I am number one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            default:
+                System.out.println("Number is not");
+
+        }
+    }
+}
