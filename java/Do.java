b18c4bc1bd373e6b96952f9ddc6c442f291ebb2a
Sivalakshmi <121811723+Sivalakshmiee@users.noreply.github.com>
2024-03-04 10:17:16 +0530

    Add files via upload

---
+++ java/Do.java
@@ -0,0 +1,12 @@
+public class Do {
+    public static void main(String[] args) {
+
+        int i = 7;
+        do {
+            System.out.println("Hi" + i);
+
+            i++;
+        } while (i < 6);
+
+    }
+}
